0143099d7a41e49a00555bde3576d23a5093c398
Marc Gibson <114510052+84Zidac@users.noreply.github.com>
2023-02-06 14:21:23 -0900

    bottles and deafgrandma added

---
+++ week-01/day1/deafgrandma.js
@@ -0,0 +1,28 @@
+function deafGrandma() {
+  let goodbye = 0;
+const prompt = require('prompt-sync')();
+console.log('HEY KID!');
+let kid = prompt();
+while (goodbye < 1){
+  if (kid === ""){
+  console.log("WHAT?!");
+  kid = prompt();
+  }
+  else if(kid == 'GOODBYE!' && goodbye < 1){
+    console.log('LEAVING SO SOON?');
+    goodbye += 1;
+    kid = prompt();
+  }
+  else if (kid.toUpperCase() != kid){
+    console.log('SPEAK UP, KID!');
+    kid = prompt();
+  }
+  else if (kid.toUpperCase() == kid){
+    console.log('NO, NOT SINCE 1946!');
+    kid = prompt();
+  }
+}
+return('LATER, SKATER!');
+}
+
+console.log(deafGrandma());

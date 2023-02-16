21d6c5cd29f459eb9646d3ea0ea456a06e65d99f
Marc Gibson <114510052+84Zidac@users.noreply.github.com>
2023-02-16 04:55:55 -0900

    day 3 work and start of day 4 assignemnts

---
+++ week-02/day4/Assignments/algo-pig-latin/python/pig_latin.py
@@ -0,0 +1,11 @@
+"""Pig latin translator"""
+
+def translate(phrase):
+    """Translates a phrase into pig latin
+
+    Args:
+      phrase: A single word, short phrase, or full sentence. String.
+
+    Returns:
+      A string which is the pig-latinified version of `phrase`.
+    """

eea74f9abf2b82620745871303f5b67ec60c19c6
Marc Gibson <114510052+84Zidac@users.noreply.github.com>
2023-03-07 05:03:20 -0900

    daily push

--- week-05/day1/assignments/sql-basics/create_schema.sql
+++ week-05/day1/assignments/sql-basics/create_schema.sql
@@ -5,5 +5,30 @@ CREATE TABLE students (
   first_name   varchar(255) NOT NULL,
   last_name    varchar(255) NOT NULL,
   birthdate    date NOT NULL,
-  address_id   integer
+  address_id   INT
 );
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id  serial PRIMARY KEY,
+  line_1  varchar(255) NOT NULL,
+  city  varchar(255) NOT NULL,
+  state varchar(255) NOT NULL,
+  zipcode INT NOT NULL
+);
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id  serial PRIMARY KEY,
+  name  varchar(255) NOT NULL,
+  credits  INT NOT NULL
+);
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id  serial PRIMARY KEY,
+  student_id  INT NOT NULL,
+  class_id  INT NOT NULL,
+  grade varchar(255)
+);
+
+
+
+

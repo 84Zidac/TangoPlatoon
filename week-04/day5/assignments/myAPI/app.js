5fecfe46bfdd8f33d722ac7a7ce55c8985a3d7b6
Marcus Gibson <114510052+84Zidac@users.noreply.github.com>
2023-03-13 04:55:35 -0800

    Delete app.js

--- week-04/day5/assignments/myAPI/app.js
+++
@@ -1,8 +0,0 @@
-let api_key = '&api_key=e50a92a8cb01d03b81d214b18d5b0dc15262d04f'
-let api_secret = '&api_secret=5b47874454d9ec613a35365debbdd3dbb9d0fdbf'
-let format = '&format=json'
-async function iOtD(){
-    let response = await axios.get(`http://astrobin.com/api/v1/imageoftheday/?limit=1${api_key}${api_secret}`);
-    let returned = response.data
-    print(returned)
-}
